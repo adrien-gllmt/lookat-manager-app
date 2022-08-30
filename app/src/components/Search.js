208504c0ef4603d5f1a3fb11fb0c6986104d8110
adrien-gllmt <adri.glmt@gmail.com>
2022-08-30 22:09:54 +0200

    Added 'search by vendor' functionality on the search page

--- app/src/components/Search.js
+++ app/src/components/Search.js
@@ -1,4 +1,3 @@
-import {MdAssignment, MdFolderShared, MdOutlineSearch, MdPaid} from "react-icons/md";
 import axios from "axios"
 import {useEffect, useState} from "react";
 
@@ -6,17 +5,27 @@ export default function Search() {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const filterProducts = (data, searchQuery) => {
+        if (!searchQuery) {
+            return data;
+        }
+
+        return data.filter((product) => {
+            const postName = product.product_vendor.toLowerCase();
+            return postName.includes(searchQuery.toLowerCase());
+        });
+    };
+    const filteredProducts = filterProducts(data, searchQuery);
+
 
     useEffect(() => {
-        let i = 0;
         async function getData() {
             try {
                 const response = await axios.get(
                     `http://localhost:8080/api/products`
                 );
-                console.log(i);
-                i++;
-                console.log(i);
                 console.log(response);
                 setData(response.data);
                 setError(null);
@@ -34,8 +43,14 @@ export default function Search() {
         <div className={"main"}>
             <section className={"search-section"}>
                 <h2>RECHERCHER</h2>
-                <p>La fonction de recherche est encore en cours de développement... <br/></p>
-                    Toutefois les données affichées sont fetch depuis l'API disponible dans le repo github !
+                <p>La fonction de recherche est encore en cours de développement... <br/>
+                    Toutefois les données affichées sont fetch depuis l'API disponible dans le repo github !</p>
+                <div className={"search-field"}>
+                    <label>
+                        <p className={"search-field-name"}>FOURNISSEUR</p>
+                    </label>
+                    <input type="search" placeholder="Search.." value={searchQuery} onInput={e => setSearchQuery(e.target.value)}/>
+                </div>
             </section>
 
             <section className={"search-result"}>
@@ -57,7 +72,7 @@ export default function Search() {
                     </thead>
 
                     <tbody>
-                    {data && data.map(({ product_id, product_vendor, product_brand, product_ref, product_color, product_size, product_gender, product_material, product_type, product_style, product_avail, product_booked }) => (
+                    {data && filteredProducts.map(({ product_id, product_vendor, product_brand, product_ref, product_color, product_size, product_gender, product_material, product_type, product_style, product_avail, product_booked }) => (
                         <tr key={product_id}>
                             <td>{product_vendor}</td>
                             <td>{product_brand}</td>

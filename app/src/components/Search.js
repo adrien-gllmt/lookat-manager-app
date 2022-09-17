0dc4224705f47d8f129e31a4e5a0f47988efd3d3
adrien-gllmt <adri.glmt@gmail.com>
2022-09-17 14:51:11 +0200

    Removed unused dependency "useMemo"

--- app/src/components/Search.js
+++ app/src/components/Search.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {useEffect, useMemo, useState} from "react";
+import {useEffect, useState} from "react";
 import Loader from "./Loader";
 import { normalizeStr } from "../utils";
 

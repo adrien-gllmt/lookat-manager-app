eb602126d3dcfc7d7a60676269b901fc30a71563
adrien-gllmt <adri.glmt@gmail.com>
2022-09-19 14:25:10 +0200

    String is now trimmed

--- app/src/utils/stringUtils.js
+++ app/src/utils/stringUtils.js
@@ -1,3 +1,3 @@
 export function normalizeStr(str) {
-    return str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    return str.trim().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
